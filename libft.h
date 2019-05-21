ae9fd07a67a988d09a576dec45422f4b0a69a700
Waseem AHMED <wasahmed@c4r11s4.wethinkcode.co.za>
2019-05-21 09:23:00 +0200

    sorted out indentation

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: wasahmed <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/20 09:08:03 by wasahmed          #+#    #+#             */
-/*   Updated: 2019/05/21 07:56:29 by wasahmed         ###   ########.fr       */
+/*   Updated: 2019/05/21 08:10:00 by wasahmed         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 # define LIBFT_H
 # include <unistd.h>
 
-	void	ft_putchar(char c);
-	void	ft_putstr(char const *s);
+void	ft_putchar(char c);
+void	ft_putstr(char const *s);
+void	ft_putnbr(int n);
 #endif
