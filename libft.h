da360415f2d2dbddd94671233199f2bd5a121aab
Waseem AHMED <wasahmed@c4r10s3.wethinkcode.co.za>
2019-05-23 08:37:24 +0200

    latest

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: wasahmed <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/20 09:08:03 by wasahmed          #+#    #+#             */
-/*   Updated: 2019/05/23 07:54:49 by wasahmed         ###   ########.fr       */
+/*   Updated: 2019/05/23 08:20:42 by wasahmed         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,5 +21,6 @@ void	ft_putnbr(int n);
 size_t	ft_strlen(const char *s);
 char	*ft_strcpy(char *dest, const char *src);
 int		ft_toupper(int c);
+int		ft_tolower(int c);
 
 #endif
