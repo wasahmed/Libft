ee3f6c2e4c4d955e34f1490c6da52d13c22cf00f
Waseem AHMED <wasahmed@c4r10s3.wethinkcode.co.za>
2019-05-29 15:00:58 +0200

    complete

--- ft_memmove.c
+++ ft_memmove.c
@@ -1,33 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: wasahmed <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/05/28 09:36:15 by wasahmed          #+#    #+#             */
-/*   Updated: 2019/05/29 13:38:25 by wasahmed         ###   ########.fr       */
+/*   Created: 2019/05/29 10:33:34 by wasahmed          #+#    #+#             */
+/*   Updated: 2019/05/29 14:59:52 by wasahmed         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdlib.h>
 
-void	*ft_memcpy(void *dst, const void *str, size_t n)
+void	*ft_memmove(void *dst, const void *src, size_t n)
 {
 	size_t	i;
-	char	*dest;
-	char	*src;
-	
-	if (str == dst)
-		return ((char *)str);
-	dest = (char *)dst;
-	src = (char *)str;
+	const char *strsrc;
+	char *strdst;
+
+	if (src == dst)
+		return (dst);
 	i = 0;
-	while (i < n)
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	return (dst);
+	strsrc = (const char *)src;
+	strdst = (char *)dst;
+	if (src < dst)
+		while (++i <= n)
+			strdst[n - i] = strsrc[n - i];
+	else
+		while (n-- > 0)
+			*(strdst++) = *(strsrc++);
+	return (strdst);
 }
