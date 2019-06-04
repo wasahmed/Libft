ccdabdc95a63ec5e21b657b0814ece1b9bd95d5c
Event WeThinkCode <event@c4r10s3.wethinkcode.co.za>
2019-06-04 11:02:56 +0200

    updated

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: wasahmed <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/20 09:08:03 by wasahmed          #+#    #+#             */
-/*   Updated: 2019/06/03 13:25:09 by wasahmed         ###   ########.fr       */
+/*   Updated: 2019/06/04 11:00:14 by event            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,5 +41,6 @@ void	*ft_memccpy(void *dst, const void *src, int c, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t n);
 char	*ft_strcat(char *s1, const char *s2);
 char	*ft_strncat(char *s1, const char *s2, size_t n);
+char	*ft_strchr(const char *s, int c);
 
 #endif
