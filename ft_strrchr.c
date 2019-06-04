cec33b238b1f7da50a08299cd46f0e7bd1cfcae4
Event WeThinkCode <event@c4r10s3.wethinkcode.co.za>
2019-06-04 11:28:06 +0200

    last char

---
+++ ft_strrchr.c
@@ -0,0 +1,16 @@
+
+#include "libft.h"
+
+char	*ft_strrchr(const char *s, int c)
+{
+	int		len;
+
+	len = ft_strlen(s);
+	while (len >= 0)
+	{
+		if (s[len] == (unsigned char)c)
+			return ((char *)s + len);
+		len--;
+	}
+	return (NULL);
+}
