e512c652f6a7e05ecc8abacce90cb1a7c69247a7
Waseem AHMED <wasahmed@c4r10s3.wethinkcode.co.za>
2019-06-17 13:54:43 +0200

    done

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -5,59 +5,58 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: wasahmed <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/06/12 14:07:45 by wasahmed          #+#    #+#             */
-/*   Updated: 2019/06/12 14:10:28 by wasahmed         ###   ########.fr       */
+/*   Created: 2019/06/17 12:46:55 by wasahmed          #+#    #+#             */
+/*   Updated: 2019/06/17 13:51:22 by wasahmed         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static size_t		ft_words(char const *s, char c)
+static	size_t	ft_wordsize(const char *str, char c)
 {
-	int			i;
-	size_t		size;
+	int		i;
+	int		size;
 
-	i = 0;
 	size = 0;
-	while (s[i] && s[i] == c)
-		i++;
-	while (s[i])
+	i = 0;
+	while (str[i])
 	{
-		while (s[i] && s[i] == c)
+		while (str[i] && str[i] == c)
 			i++;
-		if (s[i])
+		if (str[i])
 		{
-			while (s[i] && s[i] != c)
+			while (str[i] && str[i] != c)
 				i++;
 			size++;
 		}
 	}
-	return (size);
+	return (size++);
 }
 
-char				**ft_strsplit(char const *s, char c)
+char			**ft_strsplit(const char *str, char c)
 {
-	char		**tab;
-	size_t		size;
-	int			i;
-	int			start;
+	char	**tab;
+	int		i;
+	int		col;
+	int		start;
 
-	if (!s || !(tab = (char **)malloc(sizeof(char *) * (ft_words(s, c) + 1))))
-		return (NULL);
 	i = 0;
-	size = 0;
-	while (s[i])
+	col = 0;
+	tab = (char **)malloc(sizeof(char*) * ft_wordsize(str, c) + 1);
+	if (tab == NULL)
+		return (NULL);
+	while (str[i])
 	{
-		if (s[i] == c)
+		if (str[i] == c)
 			i++;
 		else
 		{
 			start = i;
-			while (s[i] && s[i] != c)
+			while (str[i] && str[i] != c)
 				i++;
-			tab[size++] = ft_strsub(s, start, i - start);
+			tab[col++] = ft_strsub(str, start, i - start);
 		}
 	}
-	tab[size] = NULL;
+	tab[col] = NULL;
 	return (tab);
 }
