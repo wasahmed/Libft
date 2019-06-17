4186d892b0c114197fed21c621ba60a55efacba3
Waseem AHMED <wasahmed@c4r10s3.wethinkcode.co.za>
2019-06-17 14:48:03 +0200

    done

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -6,13 +6,13 @@
 /*   By: wasahmed <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/17 12:46:55 by wasahmed          #+#    #+#             */
-/*   Updated: 2019/06/17 13:51:22 by wasahmed         ###   ########.fr       */
+/*   Updated: 2019/06/17 14:44:54 by wasahmed         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static	size_t	ft_wordsize(const char *str, char c)
+static	size_t	ft_words(const char *str, char c)
 {
 	int		i;
 	int		size;
@@ -42,8 +42,7 @@ char			**ft_strsplit(const char *str, char c)
 
 	i = 0;
 	col = 0;
-	tab = (char **)malloc(sizeof(char*) * ft_wordsize(str, c) + 1);
-	if (tab == NULL)
+	if (!str || !(tab = (char **)malloc(sizeof(char*) * ft_words(str, c) + 1)))
 		return (NULL);
 	while (str[i])
 	{
